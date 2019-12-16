a9d8dc9d7628c9b96df94588908d55557048f71e
Melvin Campos Casares <melvin.campos@outlook.be>
2019-12-16 12:29:39 +0100

    Reformated code

--- src/be/billfoldit/model/Member.java
+++ src/be/billfoldit/model/Member.java
@@ -3,104 +3,117 @@ package be.billfoldit.model;
 import be.billfoldit.TypeMember;
 
 /**
- * Class which create a member with a role (see enum TypeMember), first and last name.
- * This class also show the complete name of the member.
+ * Class which create a member with a role (see enum TypeMember), first and last
+ * name. This class also show the complete name of the member.
+ * 
  * @author Melvin Campos Casares
  */
 public class Member {
-    private String firstName;
-    private String lastName;
-    private TypeMember role;
+	private String firstName;
+	private String lastName;
+	private TypeMember role;
 
-    public Member(String firstName, String lastName, TypeMember role) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.role = role;
-    }
+	public Member(String firstName, String lastName, TypeMember roleEnum) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.role = roleEnum;
+	}
 
-    /**
-     * Method to get the first name of the member
-     * @return the first name of the member
-     */
-    public String getFirstName() {
-        return firstName;
-    }
+	/**
+	 * Method to get the first name of the member
+	 * 
+	 * @return the first name of the member
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
 
-    /**
-     * Method to set the first name of the member
-     * @param firstName the first name of the member
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	/**
+	 * Method to set the first name of the member
+	 * 
+	 * @param firstName the first name of the member
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    /**
-     * Method to get the last name of the member
-     * @return the last name of the member
-     */
-    public String getLastName() {
-        return lastName;
-    }
+	/**
+	 * Method to get the last name of the member
+	 * 
+	 * @return the last name of the member
+	 */
+	public String getLastName() {
+		return lastName;
+	}
 
-    /**
-     * Method to set the last name of the member
-     * @param lastName the last name of the member
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	/**
+	 * Method to set the last name of the member
+	 * 
+	 * @param lastName the last name of the member
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    /**
-     * Method to get the role of the member
-     * @return the role (see enum TypeMember) of the member
-     */
-    public TypeMember getRole() {
-        return role;
-    }
+	/**
+	 * Method to get the role of the member
+	 * 
+	 * @return the role (see enum TypeMember) of the member
+	 */
+	public TypeMember getRole() {
+		return role;
+	}
 
-    /**
-     * Method to set the role of the member
-     * @param role the role (see enum TypeMember) of the member
-     */
-    public void setRole(TypeMember role) {
-        this.role = role;
-    }
+	/**
+	 * Method to set the role of the member
+	 * 
+	 * @param role the role (see enum TypeMember) of the member
+	 */
+	public void setRole(TypeMember role) {
+		this.role = role;
+	}
 
-    /**
-     * Method to display the complete name of the member
-     * @return the first and last name of the member
-     */
-    public String displayName() {
-        return this.firstName + " " + this.lastName;
-    }
+	/**
+	 * Method to display the complete name of the member
+	 * 
+	 * @return the first and last name of the member
+	 */
+	public String displayName() {
+		return this.firstName + " " + this.lastName;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        Member member = (Member) o;
+		Member member = (Member) o;
 
-        if (!firstName.equals(member.firstName)) return false;
-        if (!lastName.equals(member.lastName)) return false;
-        return role == member.role;
-    }
+		if (!firstName.equals(member.firstName))
+			return false;
+		if (!lastName.equals(member.lastName))
+			return false;
+		return role == member.role;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = firstName.hashCode();
-        result = 31 * result + lastName.hashCode();
-        result = 31 * result + role.hashCode();
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = firstName.hashCode();
+		result = 31 * result + lastName.hashCode();
+		result = 31 * result + role.hashCode();
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", role=" + role +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Member{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", role=" + role + '}';
+	}
+
+	// This is useless but need to keep for the moment (check ConsoleView)
+	public enum TypeMember {
+		FATHER, MOTHER, SON, DAUGHTER, CHILD, FRIEND, ADULT_MALE, ADULT_FEMALE
+	}
 
 }
--- src/be/billfoldit/model/Spending.java
+++ src/be/billfoldit/model/Spending.java
@@ -1,79 +1,84 @@
 package be.billfoldit.model;
 
 /**
- * Class which create a spending based on a description and an amount.
- * The amount can be a round or unround value.
+ * Class which create a spending based on a description and an amount. The
+ * amount can be a round or unround value.
+ * 
  * @author Melvin Campos Casares
  */
 public class Spending {
-    private String description;
-    private double amount;
+	private String description;
+	private double amount;
 
-    public Spending(String description, double amount) {
-        this.description = description;
-        this.amount = amount;
-    }
+	public Spending(String description, double amount) {
+		this.description = description;
+		this.amount = amount;
+	}
 
-    /**
-     * Method to get the description of the spending
-     * @return the description of the spending
-     */
-    public String getDescription() {
-        return description;
-    }
+	/**
+	 * Method to get the description of the spending
+	 * 
+	 * @return the description of the spending
+	 */
+	public String getDescription() {
+		return description;
+	}
 
-    /**
-     * Metho to set a description on a spending
-     * @param description the description that will be set on the spending
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	/**
+	 * Metho to set a description on a spending
+	 * 
+	 * @param description the description that will be set on the spending
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    /**
-     * Method to get the amount of the spending
-     * @return the amount of the spending
-     */
-    public double getAmount() {
-        return amount;
-    }
+	/**
+	 * Method to get the amount of the spending
+	 * 
+	 * @return the amount of the spending
+	 */
+	public double getAmount() {
+		return amount;
+	}
 
-    /**
-     * Method to an amount on a spending
-     * @param amount the amount that will be set on a spending
-     */
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+	/**
+	 * Method to an amount on a spending
+	 * 
+	 * @param amount the amount that will be set on a spending
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        Spending spending = (Spending) o;
+		Spending spending = (Spending) o;
 
-        if (Double.compare(spending.amount, amount) != 0) return false;
-        return description != null ? description.equals(spending.description) : spending.description == null;
-    }
+		if (Double.compare(spending.amount, amount) != 0)
+			return false;
+		return description != null ? description.equals(spending.description) : spending.description == null;
+	}
 
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = description != null ? description.hashCode() : 0;
-        temp = Double.doubleToLongBits(amount);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = description != null ? description.hashCode() : 0;
+		temp = Double.doubleToLongBits(amount);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        var type = amount >= 0 ? "Received amount" : "Depense";
+	@Override
+	public String toString() {
+		var type = amount >= 0 ? "Received amount" : "Depense";
 
-        return type + "{" +
-                "description='" + description + '\'' +
-                ", amount=" + amount +
-                '}';
-    }
+		return type + "{" + "description='" + description + '\'' + ", amount=" + amount + '}';
+	}
 }
